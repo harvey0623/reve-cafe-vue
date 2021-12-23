fe733dba3a8116ef1a6b14f220ae01e8a1d3190d
harvey <harvey@henghung.com>
2021-12-23 13:50:16 +0800

    add file

---
+++ src/component/Footer/index.vue
@@ -0,0 +1,15 @@
+<template>
+   <footer>
+      <div class="my-container">footer</div>
+   </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/component/Header/index.vue
@@ -0,0 +1,41 @@
+<template>
+   <header>
+      <div class="header-top">
+         <div class="my-container">
+           
+         </div>
+      </div>
+      <div class="header-bottom">
+         <div class="my-container">
+            <router-link to="/" class="logo-box">
+               <img :src="require('@/assets/img/header-logo.png')" alt="">
+            </router-link>
+            <div class="search-block">
+               <div class="input-box">
+                  <input type="text" class="my-input" placeholder="Search">
+                  <i class="fal fa-search"></i>
+               </div>
+            </div>
+            <div class="feature-block">
+               <router-link to="/" class="feature-item">
+                  <i class="fal fa-user"></i>
+                  <p class="text-primary">會員中心</p>
+               </router-link>
+               <router-link to="/" class="feature-item">
+                  <i class="fal fa-shopping-cart"></i>
+                  <p class="text-primary">購物車</p>
+                  <span class="cart-count">10</span>
+               </router-link>
+            </div>
+         </div>
+      </div>
+   </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" src="./index.scss" scoped></style>
