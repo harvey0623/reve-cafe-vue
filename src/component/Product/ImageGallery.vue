3f675751beb77550ae16772de9b55355019eccaa
harvey <harvey@henghung.com>
2022-01-10 14:38:57 +0800

    完成商品詳情基本ui

---
+++ src/component/Product/ImageGallery.vue
@@ -0,0 +1,117 @@
+<template>
+   <div class="product-gallery">
+      <div class="swiper main-swiper">
+         <div class="swiper-wrapper">
+            <div class="swiper-slide">
+               <img src="https://swiperjs.com/demos/images/nature-1.jpg">
+            </div>
+            <div class="swiper-slide">
+               <img src="https://swiperjs.com/demos/images/nature-2.jpg">
+            </div>
+            <div class="swiper-slide">
+               <img src="https://swiperjs.com/demos/images/nature-3.jpg">
+            </div>
+            <div class="swiper-slide">
+               <img src="https://swiperjs.com/demos/images/nature-4.jpg">
+            </div>
+            <div class="swiper-slide">
+               <img src="https://swiperjs.com/demos/images/nature-5.jpg">
+            </div>
+         </div>
+      </div>
+      <div class="swiper sub-swiper">
+         <div class="swiper-wrapper">
+            <div class="swiper-slide">
+               <div class="imgBox">
+                  <img src="https://swiperjs.com/demos/images/nature-1.jpg">
+               </div>
+            </div>
+            <div class="swiper-slide">
+               <div class="imgBox">
+                  <img src="https://swiperjs.com/demos/images/nature-2.jpg">
+               </div>
+            </div>
+            <div class="swiper-slide">
+               <div class="imgBox">
+                  <img src="https://swiperjs.com/demos/images/nature-3.jpg">
+               </div>
+            </div>
+            <div class="swiper-slide">
+               <div class="imgBox">
+                  <img src="https://swiperjs.com/demos/images/nature-4.jpg">
+               </div>
+            </div>
+            <div class="swiper-slide">
+               <div class="imgBox">
+                  <img src="https://swiperjs.com/demos/images/nature-5.jpg">
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</template>
+
+<script>
+import { ref, reactive, onMounted } from "@vue/composition-api"
+import Swiper from 'swiper/swiper-bundle.min.js'
+export default {
+   props: {},
+   setup(props, { root }) {
+
+      let initSwiper = () => {
+         let subSwiper = new Swiper('.sub-swiper', {
+            slidesPerView: 4,
+            freeMode: true,
+            watchSlidesProgress: true,
+            spaceBetween: 3,
+         });
+         new Swiper('.main-swiper', {
+            thumbs: {
+               swiper: subSwiper
+            }
+         });
+      }
+
+      onMounted(() => {
+         initSwiper()
+      })
+
+      return {};
+   },
+};
+</script>
+
+<style lang="scss">
+.main-swiper {
+   margin-bottom: 5px;
+   .swiper-slide {
+      position: relative;
+      padding-top: 75%;
+      img {
+         position: absolute;
+         left: 0;
+         top: 0;
+         @include size(100%);
+      }
+   }
+}
+.sub-swiper {
+   .swiper-slide {
+      border: 3px solid transparent;
+      &.swiper-slide-thumb-active{
+         border-color: map-get($borderColor, primary);
+      }
+      .imgBox {
+         position: relative;
+         padding-top: 75%;
+         cursor: pointer;
+         img {
+            position: absolute;
+            left: 0;
+            top: 0;
+            @include size(100%);
+         }
+      }
+   }
+}
+</style>
--- src/views/product/detail.vue
+++ src/views/product/detail.vue
@@ -1,14 +1,52 @@
 <template>
    <div class="product_detail">
-
+      <h3 class="categoryName text-primary">商品名稱</h3>
+      <div class="product-top">
+         <div class="slideBox">
+            <ImageGallery></ImageGallery>
+         </div>
+         <div class="descBox">
+            <p class="productNum">xasxsaxas</p>
+            <p class="productName">xasxasxsa</p>
+            <p class="productSummary">xasxsaxasxsa</p>
+            <ul class="specList">
+               <li class="spec-item active">巴西咖啡</li>
+               <li class="spec-item">巴西咖啡</li>
+            </ul>
+            <div class="edit-block">
+               <div class="edit-row">
+                  <span>溫層:</span>
+                  <span>常溫</span>
+               </div>
+               <div class="edit-row">
+                  <span>數量:</span>
+                  <select class="form-control">
+                     <option value="1">1</option>
+                  </select>
+               </div>
+               <div class="edit-row">
+                  <span>價格:</span>
+                  <span class="price text-primary">${{ 1000 | currency }}</span>
+               </div>
+               <div class="edit-row btnBox no-mt">
+                  <button class="cart">加入購物車</button>
+                  <button class="immediate">直接購買</button>
+               </div>
+            </div>
+         </div>
+      </div>
+      <div class="product-middle">
+         xsaxasxas
+      </div>
    </div>
 </template>
 
 <script>
 import { ref, reactive, computed, onMounted, watch, onBeforeMount } from '@vue/composition-api'
+import ImageGallery from '@/component/Product/ImageGallery.vue';
 export default {
    name: 'product-detail',
-   components: {},
+   components: { ImageGallery },
    metaInfo () {
       return {
         title: '商品詳情',
@@ -35,6 +73,4 @@ export default {
 }
 </script>
 
-<style>
-
-</style>
+<style src="./scss/detail.scss" lang="scss"></style>
