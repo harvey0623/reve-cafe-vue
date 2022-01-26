56ba023c492f19cff8b1bae855679bf685443031
harvey <harvey@henghung.com>
2022-01-26 11:48:23 +0800

    移除掉因composition-api整合而mark掉的code

--- src/views/activity/quanity.vue
+++ src/views/activity/quanity.vue
@@ -1,9 +1,8 @@
 <template src="./html/quanity.html"></template>
 
 <script>
-import { ref, reactive, computed, onMounted, watch } from '@vue/composition-api'
+import { computed } from '@vue/composition-api'
 import { createSalesInfo } from '@/composition-api/index.js'
-// import { activityCartApi } from '@/api/index.js'
 import fullAmountItem from '@/component/ActivityProduct/fullAmountItem.vue'
 import fullAmountRow from '@/component/ActivityRow/FullAmountRow.vue'
 export default {
@@ -16,39 +15,6 @@ export default {
    },
    setup(props, { root }) {
       let { isLoading, productList, pickedList, cartMessage, cartModal, productModal, activityId, criteria, hasProduct, pickedCount, pickedTotalDollar, discountText, getActivityProduct, pickedHandler, changeSpecCount, removePickedItem, introHandler, addCart } = createSalesInfo(root, 'full_amount_meta');
-      // let isLoading = ref(false);
-      // let productList = reactive({ data: [] });
-      // let pickedList = reactive({ data: [] });
-      // let cartMessage = ref('');
-      // let cartModal = ref(null);
-      // let activityId = computed(() => root.$store.state.activity.activityInfo.id);
-      // let criteria = computed(() => root.$store.getters['activity/criteria']);
-
-      // let pickedCount = computed(() => {
-      //    return pickedList.data.reduce((prev, current) => {
-      //       prev += current.buyCount;
-      //       return prev; 
-      //    }, 0);
-      // });
-
-      // let pickedTotalDollar = computed(() => {
-      //    return pickedList.data.reduce((prev, current) => {
-      //       prev += current.buyCount * current.price;
-      //       return prev;
-      //    }, 0);
-      // });
-      
-      // let discountText = computed(() => {
-      //    let { status, iPromoAmount, iPromoType } = criteria.value;
-      //    if (status === 0) return '';
-      //    let currencyFormat = root.$options.filters.currency;
-      //    let mappingText = {
-      //       1: `固定優惠${currencyFormat(iPromoAmount)}元`,
-      //       2: `折扣優惠${currencyFormat(iPromoAmount)}元`,
-      //       3: `折扣優惠${iPromoAmount}%`
-      //    };
-      //    return `已符合滿額折扣(${mappingText[iPromoType]})`;
-      // });
 
       let isAchieved = computed(() => {
          if (criteria.value.status === 0) return false;
@@ -70,91 +36,6 @@ export default {
             return 0;
          }
       });
-
-      // let createProductList = (arr) => {
-      //    let list = arr.filter(item => {
-      //       let isNotNull = item.product !== null && item.full_amount_meta_spec !== null;
-      //       if (!isNotNull) return false;
-      //       else return item.full_amount_meta_spec.iSpecStock > 0;
-      //    });
-      //    list.forEach(item => item.buyCount = 0);
-      //    return list;
-      // }
-
-      // let getActivityProduct = async() => {
-      //    let response = await activityApi.full_amount_meta({ activity_product_promotions_ids: activityId.value });
-      //    productList.data = createProductList(response.aaData);
-      //    pickedList.data = [];
-      // }
-
-      // let setProductBuyCount = (payload) => {
-      //    let productObj = productList.data.find(item => item.iId === payload.iId);
-      //    if (productObj === undefined) return;
-      //    productObj.buyCount = payload.count;
-      // }
-
-      // let pickedHandler = (payload) => {
-      //    let { iId } = payload;
-      //    let pickedObj = pickedList.data.find(item => item.iId === iId);
-      //    if (pickedObj === undefined) {
-      //       pickedList.data.push({ ...payload, buyCount: 1 });
-      //       setProductBuyCount({ iId, count: 1 });
-      //    } else {
-      //       pickedObj.buyCount += 1;
-      //       setProductBuyCount({ iId, count: pickedObj.buyCount });
-      //    }
-      // }
-
-      // let changeSpecCount = (payload) => {
-      //    let pickedObj = pickedList.data.find(item => item.iId === payload.iId);
-      //    if (pickedObj === undefined) return;
-      //    pickedObj.buyCount = payload.count;
-      //    setProductBuyCount({ iId: payload.iId, count: payload.count });
-      // }
-
-      // let removePickedItem = (iId) => {
-      //    let index = pickedList.data.find(item => item.iId === iId);
-      //    if (index === -1) return;
-      //    pickedList.data.splice(index, 1);
-      //    setProductBuyCount({ iId, count: 0 });
-      // }
-
-      // let createCartParams = (promotionId, code) => {
-      //    let products = pickedList.data.reduce((prev, current) => {
-      //       let { productCode, specId, buyCount } = current;
-      //       prev.push({ vProductCode: productCode, iSpecId: specId, iCount: buyCount });
-      //       return prev;
-      //    }, []);
-      //    return { iActivityProductPromotionsId: promotionId, code, products };
-      // }
-
-      // let addCart = async() => {
-      //    if (!isAchieved.value) return;
-      //    isLoading.value = true;
-      //    let cartParams = createCartParams(activityId.value, 'full_amount');
-      //    let cartResponse = await activityCartApi.addCart(cartParams);
-      //    if (cartResponse.status === 1) {
-      //       cartMessage.value = cartResponse.message;
-      //       pickedList.data = [];
-      //       productList.data.forEach(item => item.buyCount = 0);
-      //       root.$store.dispatch('cart/getAllCart');
-      //    }
-      //    cartModal.value.openModal();
-      //    isLoading.value = false;
-      // }
-
-      // onMounted(async() => {
-      //    isLoading.value = true;
-      //    await getActivityProduct(full_amount_meta);
-      //    isLoading.value = false;
-      // });
-
-      // watch(activityId, async() => {
-      //    isLoading.value = true;
-      //    await getActivityProduct(full_amount_meta);
-      //    isLoading.value = false;
-      // });
-
 
       return { isLoading, productList, pickedList, cartMessage, cartModal, productModal, activityId, criteria, hasProduct, pickedCount, pickedTotalDollar, discountText, getActivityProduct, pickedHandler, changeSpecCount, removePickedItem, introHandler, addCart, isAchieved, discountPrice }
    }
--- src/views/activity/red_with_green.vue
+++ src/views/activity/red_with_green.vue
@@ -1,8 +1,7 @@
 <template src="./html/redwithgreen.html"></template>
 
 <script>
-import { ref, reactive, computed, onMounted, watch } from '@vue/composition-api'
-// import { activityApi, activityCartApi } from '@/api/index.js'
+import { ref, reactive, computed } from '@vue/composition-api'
 import { createSalesInfo } from '@/composition-api/index.js'
 import MarkItem from '@/component/MarkItem/index.vue'
 import RedGreenItem from '@/component/ActivityProduct/redGreenItem.vue'
@@ -17,11 +16,6 @@ export default {
    },
    setup(props, { root }) {
       let { isLoading, productList, pickedList, cartMessage, cartModal, productModal, activityId, criteria, hasProduct, pickedTotalDollar, pickedHandler, changeSpecCount, removePickedItem, introHandler, addCart } = createSalesInfo(root, 'red_with_green_meta');
-      // let isLoading = ref(false);
-      // let productList = reactive({ data: [] });
-      // let pickedList = reactive({ data: [] });
-      // let cartMessage = ref('');
-      // let cartModal = ref(null);
       let markType = ref('0');
       let markList = reactive({ 
          0: { title: '全部', fontColor: '#BC9F75', borderColor: '#BC9F75', backgroundColor: '#fff', hoverFontColor: '#fff', hoverBackgroundColor: '#BC9F75' },
@@ -33,20 +27,10 @@ export default {
          2: { title: '綠', backgroundColor: '#04a978' }
       });
 
-      // let activityId = computed(() => root.$store.state.activity.activityInfo.id);
-      // let criteria = computed(() => root.$store.getters['activity/criteria']);
-
       let filterProductList = computed(() => {
          if (markType.value === '0') return productList.data;
          return productList.data.filter(item => item.iPromoType === parseInt(markType.value));
       });
-
-      // let pickedTotalDollar = computed(() => {
-      //    return pickedList.data.reduce((prev, current) => {
-      //       prev += current.buyCount * current.price;
-      //       return prev;
-      //    }, 0);
-      // });
 
       let redItemCount = computed(() => {
          return pickedList.data.reduce((prev, current) => {
@@ -68,91 +52,7 @@ export default {
          let { iRedAmount, iGreenAmount } = criteria.value;
          return redItemCount.value === iRedAmount && greenItemCount.value === iGreenAmount;
       });
-
-      // let createProductList = (arr) => {
-      //    let list = arr.filter(item => {
-      //       let isNotNull = item.product !== null && item.full_amount_meta_spec !== null;
-      //       if (!isNotNull) return false;
-      //       else return item.full_amount_meta_spec.iSpecStock > 0;
-      //    });
-      //    list.forEach(item => item.buyCount = 0);
-      //    return list;
-      // }
-
-      // let setProductBuyCount = (payload) => {
-      //    let productObj = productList.data.find(item => item.iId === payload.iId);
-      //    if (productObj === undefined) return;
-      //    productObj.buyCount = payload.count;
-      // }
-
-      // let pickedHandler = (payload) => {
-      //    let { iId } = payload;
-      //    let pickedObj = pickedList.data.find(item => item.iId === iId);
-      //    if (pickedObj === undefined) {
-      //       pickedList.data.push({ ...payload, buyCount: 1 });
-      //       setProductBuyCount({ iId, count: 1 });
-      //    } else {
-      //       pickedObj.buyCount += 1;
-      //       setProductBuyCount({ iId, count: pickedObj.buyCount });
-      //    }
-      // }
-
-      // let getActivityProduct = async() => {
-      //    let response = await activityApi.red_with_green_meta({ activity_product_promotions_ids: activityId.value });
-      //    productList.data = createProductList(response.aaData);
-      //    pickedList.data = [];
-      // }
-
-      // let changeSpecCount = (payload) => {
-      //    let pickedObj = pickedList.data.find(item => item.iId === payload.iId);
-      //    if (pickedObj === undefined) return;
-      //    pickedObj.buyCount = payload.count;
-      //    setProductBuyCount({ iId: payload.iId, count: payload.count });
-      // }
-
-      // let removePickedItem = (iId) => {
-      //    let index = pickedList.data.find(item => item.iId === iId);
-      //    if (index === -1) return;
-      //    pickedList.data.splice(index, 1);
-      //    setProductBuyCount({ iId, count: 0 });
-      // }
-
-      // let createCartParams = (promotionId, code) => {
-      //    let products = pickedList.data.reduce((prev, current) => {
-      //       let { productCode, specId, buyCount } = current;
-      //       prev.push({ vProductCode: productCode, iSpecId: specId, iCount: buyCount });
-      //       return prev;
-      //    }, []);
-      //    return { iActivityProductPromotionsId: promotionId, code, products };
-      // }
-
-      // let addCart = async() => {
-      //    if (!isAchieved.value) return;
-      //    isLoading.value = true;
-      //    let cartParams = createCartParams(activityId.value, 'red_with_green');
-      //    let cartResponse = await activityCartApi.addCart(cartParams);
-      //    if (cartResponse.status === 1) {
-      //       cartMessage.value = cartResponse.message;
-      //       pickedList.data = [];
-      //       productList.data.forEach(item => item.buyCount = 0);
-      //       root.$store.dispatch('cart/getAllCart');
-      //    }
-      //    cartModal.value.openModal();
-      //    isLoading.value = false;
-      // }
-
-      // onMounted(async() => {
-      //    isLoading.value = true;
-      //    await getActivityProduct();
-      //    isLoading.value = false;
-      // });
-
-      // watch(activityId, async() => {
-      //    isLoading.value = true;
-      //    await getActivityProduct();
-      //    isLoading.value = false;
-      // });
-
+      
       return { isLoading, pickedList,cartMessage, activityId, criteria, cartModal, productModal, markType, markList, filterProductList, pickedHandler, changeSpecCount, removePickedItem, pickedTotalDollar, hasProduct, redItemCount, greenItemCount, isAchieved, introHandler, introHandler, addCart, mappingLabel }
    }
 }
