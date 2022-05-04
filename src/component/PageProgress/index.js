f0417744b8c7e2d069ba7f3cd5be0e3b6ce47437
harvey <harvey@henghung.com>
2022-05-04 15:25:53 +0800

    開啟router beforeEach驗證

--- src/component/PageProgress/index.js
+++ src/component/PageProgress/index.js
@@ -3,5 +3,6 @@ import VueProgressBar from 'vue-progressbar'
 Vue.use(VueProgressBar, {
   color: '#bc9f75',
   failedColor: '#bc9f75',
-  thickness: '2px'
+  thickness: '2px',
+  autoFinish: false
 })
